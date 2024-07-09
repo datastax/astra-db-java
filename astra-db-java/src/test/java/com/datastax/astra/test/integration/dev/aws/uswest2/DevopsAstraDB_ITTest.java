b1d29ddb02424c9b4fed1498801f5866a4ab3e42
Cedrick Lunven <cedrick.lunven@gmail.com>
2024-07-09 20:39:50 +0200

    test

--- astra-db-java/src/test/java/com/datastax/astra/test/integration/dev/aws/uswest2/DevopsAstraDB_ITTest.java
+++ astra-db-java/src/test/java/com/datastax/astra/test/integration/dev/aws/uswest2/DevopsAstraDB_ITTest.java
@@ -1,18 +1,14 @@
 package com.datastax.astra.test.integration.dev.aws.uswest2;
 
-import com.datastax.astra.client.admin.AstraDBAdmin;
-import com.datastax.astra.client.admin.DatabaseAdmin;
 import com.datastax.astra.test.integration.AbstractAstraDBAdminTest;
-import com.datastax.astra.test.integration.TestDataSet;
 import com.dtsx.astra.sdk.db.domain.CloudProviderType;
 import com.dtsx.astra.sdk.utils.AstraEnvironment;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @EnabledIfEnvironmentVariable(named = "ASTRA_DB_APPLICATION_TOKEN", matches = "Astra.*")
-public class DevopsAstraDB_TTest extends AbstractAstraDBAdminTest {
+public class DevopsAstraDB_ITTest extends AbstractAstraDBAdminTest {
 
     @Override
     protected AstraEnvironment getAstraEnvironment() {
