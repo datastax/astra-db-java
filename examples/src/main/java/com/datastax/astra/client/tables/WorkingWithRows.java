e789bfae49dbb44aa68d3d92c7cf01e5827bcf46
Cedrick Lunven <cedrick.lunven@gmail.com>
2024-12-04 16:04:17 +0100

    update examples

--- examples/src/main/java/com/datastax/astra/client/tables/WorkingWithRows.java
+++ examples/src/main/java/com/datastax/astra/client/tables/WorkingWithRows.java
@@ -46,19 +46,19 @@ public class WorkingWithRows {
 
     public static void insertFullRow() {
         Database db = DataAPIClients.localDbWithDefaultKeyspace();
+        // cf CreateTable.java
         Table<Row> table = db.getTable("games");
-//        table.insertOne(new Row()
-//                .addText("match_id", "mtch_0")
-//                .addInt("round", 1)
-//                .addVector("m_vector", new DataAPIVector(new float[]{0.4f, -0.6f, 0.2f}))
-//                .addInt("score", 18)
-//                .addTimeStamp("when", Instant.now())
-//                .addText("winner", "Victor")
-//                .addSet("fighters", Set.of(UUID.fromString("0193539a-2770-8c09-a32a-111111111111"))));
+        table.insertOne(new Row()
+          .addText("match_id", "mtch_0")
+          .addInt("round", 1)
+          .addVector("m_vector", new DataAPIVector(new float[]{0.4f, -0.6f, 0.2f}))
+          .addInt("score", 18)
+          .addTimeStamp("when", Instant.now())
+          .addText("winner", "Victor")
+          .addSet("fighters", Set.of(UUID.fromString("0193539a-2770-8c09-a32a-111111111111"))));
+
 
         // Leveraging object mapping
-        Table<Game> table2 = db.getTable("games", Game.class);
-
         Game match1 = new Game()
                 .matchId("mtch_1")
                 .round(2)
@@ -67,11 +67,11 @@ public class WorkingWithRows {
                 .winner("Victor")
                 .fighters(Set.of(UUID.fromString("0193539a-2770-8c09-a32a-111111111111")))
                 .vector(new DataAPIVector(new float[]{0.4f, -0.6f, 0.2f}));
+        db.getTable("games", Game.class).insertOne(match1);
+
+        // The convertValue will serialize twice with $binary
         Row row = new RowSerializer().convertValue(match1, Row.class);
-        System.out.println(row.get("vector"));
-        System.out.println(((Map<String, Object>) row.get("vector")).get("$binary").getClass());
-        System.out.println();
-        //table2.insertOne(match1);
+        System.out.println(row.toString());
     }
 
 }
