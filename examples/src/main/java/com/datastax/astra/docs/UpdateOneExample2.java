9548ffc86de963cad87c063fee84f3a292711fc5
Cedrick Lunven <cedrick.lunven@gmail.com>
2025-03-12 00:10:47 +0100

    FindAndRerank

--- examples/src/main/java/com/datastax/astra/docs/UpdateOneExample2.java
+++ examples/src/main/java/com/datastax/astra/docs/UpdateOneExample2.java
@@ -52,6 +52,7 @@ public class UpdateOneExample2 {
         File file = new File(Main.class.getClassLoader().getResource("quickstart_dataset.json").toURI());
         List<Book> books = new ObjectMapper().readValue(new FileInputStream(file), new TypeReference<>() {});
 
+        // Populate the Table
         /**
          * Ideally we would do
          * table.insertMany(books);
@@ -69,119 +70,9 @@ public class UpdateOneExample2 {
         }).toList();
         database.getTable("quickstart_table").insertMany(rows);
 
-        //updateTable(table);
-//        Filter filter = Filters.and(
-//                Filters.eq("title", "Hidden Shadows of the Past"),
-//                Filters.eq("author", "John Anthony"));
-//        table.find(filter).toList().forEach(row -> System.out.println(row));
-    }
 
-    public static void populateTable(Table<Row> table) throws Exception {
-        // Initialize Jackson ObjectMapper
-        ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File(Main.class.getClassLoader().getResource("quickstart_dataset.json").toURI());
+        // Next Seach Table
 
-        try (FileInputStream stream = new FileInputStream(file)) {
-            List<Row> rows = objectMapper.readValue(stream, new TypeReference<>() {});
-            rows.forEach(
-                    row -> {
-                        // Deserialize the "genres" field into a HashSet
-                        row.add("genres", new HashSet<>(row.getList("genres", String.class)));
-
-                        // Deserialize the "metadata" field into a Map
-                        Map<String, String> metadataMap =
-                                objectMapper.convertValue(row.get("metadata"), new TypeReference<Map<String, String>>() {});
-                        row.add("metadata", metadataMap);
-
-                        // Deserialize the "dueDate" field into a Date or null
-                        row.add("dueDate", parseDate(row.getText("dueDate")));
-
-                        // Add a field of text to vectorize
-                        String summary = row.getText("summary");
-                        String genres = String.join(", ", row.getList("genres", String.class));
-                        String summaryGenresVector =
-                                String.format("summary: %s | genres: %s", summary, genres);
-                        row.add("summaryGenresVector", summaryGenresVector);
-                    });
-
-            TableInsertManyResult result = table.insertMany(rows);
-            System.out.println("Inserted " + result.getInsertedIds().size() + " items.");
-        }
-    }
-
-    public static void searchTable(Table<Row> table) {
-        // Find rows that match a filter
-        System.out.println("\nFinding books with rating greater than 4.7...");
-
-        Filter filter = Filters.gt("rating", 4.7);
-
-        TableFindOptions options = new TableFindOptions()
-                .limit(10)
-                .projection(include("title", "rating"));
-
-        table
-                .find(filter, options)
-                .forEach(
-                        row -> {
-                            System.out.println(row.get("title") + " is rated " + row.get("rating"));
-                        });
-
-        // Perform a vector search to find the closest match to a search string
-        System.out.println("\nUsing vector search to find a single scary novel...");
-
-        TableFindOneOptions options2 =
-                new TableFindOneOptions()
-                        .sort(Sort.vectorize("summaryGenresVector", "A scary novel"))
-                        .projection(include("title"));
-
-        table
-                .findOne(options2)
-                .ifPresent(
-                        row -> {
-                            System.out.println(row.get("title") + " is a scary novel");
-                        });
-
-        // Combine a filter, vector search, and projection to find the 3 books with
-        // more than 400 pages that are the closest matches to a search string
-        System.out.println(
-                "\nUsing filters and vector search to find 3 books with more than 400 pages that are set in the arctic, returning just the title and author...");
-
-        Filter filter3 = Filters.gt("numberOfPages", 400);
-
-        TableFindOptions options3 =
-                new TableFindOptions()
-                        .limit(3)
-                        .sort(Sort.vectorize("summaryGenresVector", "A book set in the arctic"))
-                        .projection(include("title", "author"));
-
-        table
-                .find(filter3, options3)
-                .forEach(
-                        row -> {
-                            System.out.println(row);
-                        });
-    }
-
-    public static void updateTable(Table<Row> table) {
-        // Update a row
-        Filter filter = new Filter(Map.of(
-                "title", "Hidden Shadows of the Past",
-                "author", "John Anthony"));
-        TableUpdateOperation update = new TableUpdateOperation()
-                .set("rating", 4.5)
-                .set("genres", Arrays.asList("Fiction", "Drama"))
-                .unset("borrower");
-        table.updateOne(filter, update);
-    }
-
-
-    private static Date parseDate(String date) {
-        if (date == null) return null;
-        try {
-            return new SimpleDateFormat("yyyy-MM-dd").parse(date);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
     }
 
 }
