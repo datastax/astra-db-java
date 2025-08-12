5ef47f058fed80a623b160b42c0876abd9fe0e19
Cedrick Lunven <cedrick.lunven@gmail.com>
2025-08-12 13:26:56 +0200

    update samples

---
+++ examples/src/main/java/com/datastax/astra/client/tables/CreateIndexOnValues.java
@@ -0,0 +1,54 @@
+package com.datastax.astra.client.tables;
+
+import com.datastax.astra.client.DataAPIClient;
+import com.datastax.astra.client.DataAPIDestination;
+import com.datastax.astra.client.core.auth.UsernamePasswordTokenProvider;
+import com.datastax.astra.client.core.options.DataAPIClientOptions;
+import com.datastax.astra.client.databases.Database;
+import com.datastax.astra.client.tables.commands.options.CreateIndexOptions;
+import com.datastax.astra.client.tables.commands.options.CreateTableOptions;
+import com.datastax.astra.client.tables.definition.TableDefinition;
+import com.datastax.astra.client.tables.definition.columns.TableColumnTypes;
+import com.datastax.astra.client.tables.definition.indexes.TableIndexMapTypes;
+import com.datastax.astra.client.tables.definition.rows.Row;
+
+public class CreateIndexOnValues {
+
+    public static Database getHCDDatabase(String url, String username, String password, String keyspace) {
+        String authToken = new UsernamePasswordTokenProvider(username, password)
+                .getToken();
+        DataAPIClientOptions options = new DataAPIClientOptions()
+                .destination(DataAPIDestination.HCD)
+                .enableFeatureFlagTables()
+                .logRequests();
+        DataAPIClient client = new DataAPIClient(authToken,options);
+        return client.getDatabase(url, keyspace);
+    }
+
+    public static void main(String[] args) {
+
+        Database database = getHCDDatabase(
+                "http://localhost:8181",
+                "cassandra",
+                "cassandra",
+                "quickstart_keyspace");
+
+        // Create a Table with a Map
+        TableDefinition tableDefinition = new TableDefinition()
+          .addColumnText("email")
+          .addColumnMap("example_map_column", TableColumnTypes.TEXT, TableColumnTypes.TEXT)
+          .partitionKey("email");
+
+        Table<Row> table = database
+          .createTable("example_index_table2",
+                  tableDefinition,
+                  CreateTableOptions.IF_NOT_EXISTS);
+
+        // Index a column
+        table.createIndex(
+                "example_index_name3",
+                "example_map_column",
+                TableIndexMapTypes.VALUES,
+                CreateIndexOptions.IF_NOT_EXISTS);
+    }
+}
