2c2a2af3f3b377e6c3a82a1a3ab5b1e21f2f7e8e
toptobes <kavinpg@gmail.com>
2025-12-02 00:31:57 -0600

    resolve #58

--- astra-sdk-devops/src/main/java/com/dtsx/astra/sdk/org/domain/IamToken.java
+++ astra-sdk-devops/src/main/java/com/dtsx/astra/sdk/org/domain/IamToken.java
@@ -6,21 +6,22 @@ import java.util.List;
  * Token used to work with Astra.
  */
 public class IamToken {
-    
     /** client id. */
     private String clientId;
-    
     /** roles list. */
     private List<String> roles;
-    
     /** generated date. */
     private String generatedOn;
+    /** description (maybe empty). */
+    private String description;
+    /** expiry date, if applicable. */
+    private String tokenExpiry;
 
     /**
      * Default constructor.
      */
     public IamToken() {}
-    
+
     /**
      * Getter accessor for attribute 'clientId'.
      *
@@ -78,4 +79,41 @@ public class IamToken {
         this.generatedOn = generatedOn;
     }
 
+    /**
+     * Getter accessor for attribute 'description'.
+     *
+     * @return
+     *       current value of 'description'
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Setter accessor for attribute 'description'.
+     * @param description
+     * 		new value for 'description '
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Getter accessor for attribute 'tokenExpiry'.
+     *
+     * @return
+     *       current value of 'tokenExpiry'
+     */
+    public String getTokenExpiry() {
+        return tokenExpiry;
+    }
+
+    /**
+     * Setter accessor for attribute 'tokenExpiry'.
+     * @param tokenExpiry
+     * 		new value for 'tokenExpiry '
+     */
+    public void setTokenExpiry(String tokenExpiry) {
+        this.tokenExpiry = tokenExpiry;
+    }
 }
